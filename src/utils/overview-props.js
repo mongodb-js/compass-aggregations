31f89fe364bac68b470e623d6aa1271e46617da2
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-02-14 20:09:22 -0500

    Delete overview-props.js

--- src/utils/overview-props.js
+++
@@ -1,5 +0,0 @@
-overview={this.props.overview}
-overviewStageModified={this.props.overviewStageModified}
-overviewStageAdded={this.props.overviewStageAdded}
-overviewStageRemoved={this.props.overviewStageAdded}
-overviewToggled={this.props.overviewToggled}
