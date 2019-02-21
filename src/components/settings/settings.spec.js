c5e84369919dfa361dfdbc0d7cb49227fe2c9162
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-02-21 17:30:52 -0500

    more tests for `<Settings />` rendering

--- src/components/settings/settings.spec.js
+++ src/components/settings/settings.spec.js
@@ -2,34 +2,54 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Settings from './settings.jsx';
+import { INITIAL_STATE } from 'modules/settings';
+
 import styles from './settings.less';
 
 describe('Settings [Component]', () => {
-  var state, component, saveSettingsSpy;
+  let state, component;
+  let saveSettingsSpy;
+  let toggleSettingsIsExpandedSpy;
+  let toggleSettingsIsCommentModeSpy;
+  let setSettingsSampleSizeSpy;
+  let setSettingsMaxTimeoutMSSpy;
+  let setSettingsLimitSpy;
 
   beforeEach(() => {
     saveSettingsSpy = sinon.spy();
+    toggleSettingsIsExpandedSpy = sinon.spy();
+    toggleSettingsIsCommentModeSpy = sinon.spy();
+    setSettingsSampleSizeSpy = sinon.spy();
+    setSettingsMaxTimeoutMSSpy = sinon.spy();
+    setSettingsLimitSpy = sinon.spy();
 
     state = {
-      maxTimeMS: 5000,
-      sampleModeEnabled: true,
-      sampleSize: 1000
+      ...INITIAL_STATE,
+      saveSettings: saveSettingsSpy,
+      toggleSettingsIsExpanded: toggleSettingsIsExpandedSpy,
+      toggleSettingsIsCommentMode: toggleSettingsIsCommentModeSpy,
+      setSettingsSampleSize: setSettingsSampleSizeSpy,
+      setSettingsMaxTimeoutMS: setSettingsMaxTimeoutMSSpy,
+      setSettingsLimit: setSettingsLimitSpy
     };
-
-    component = shallow(
-      <Settings
-        saveSettings={saveSettingsSpy}
-        {...state}
-      />
-    );
   });
 
   afterEach(() => {
     component = null;
-    saveSettingsSpy = null;
+    toggleSettingsIsExpandedSpy = null;
+    toggleSettingsIsCommentModeSpy = null;
+    setSettingsSampleSizeSpy = null;
+    setSettingsMaxTimeoutMSSpy = null;
+    setSettingsLimitSpy = null;
   });
 
-  it('renders the wrapper div', () => {
+  it('is hidden by default', () => {
+    component = shallow(<Settings {...state} />);
+    expect(Object.keys(component).length).to.equal(0);
+  });
+
+  it('is rendered when isExpanded=true', () => {
+    component = shallow(<Settings {...{ ...state, isExpanded: true }} />);
     expect(component.find(`.${styles.settings}`)).to.be.present();
   });
 });
