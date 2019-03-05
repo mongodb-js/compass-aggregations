1d8f1589134e92f34554f130c85e048dce98b9dc
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-03-05 14:30:49 -0500

    COMPASS-3410 Setup StageEditor story to work in

---
+++ examples/stage-editor.stories.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { ComponentPreview } from 'storybook/decorators';
+
+import { Provider } from 'react-redux';
+import { INITIAL_STATE } from 'modules';
+import { configureStore } from 'utils/configureStore';
+
+import { STAGE_DEFAULTS } from './example-constants.js';
+
+import { ObjectId } from 'bson';
+
+import { action } from '@storybook/addon-actions';
+
+const BASE_STATE = {
+  ...INITIAL_STATE
+};
+
+const PROPS = {
+    ...STAGE_DEFAULTS,
+    runStage: action('runStage'),
+    stageChanged: action('stageChanged'),
+    setIsModified: action('setIsModified'),
+    fromStageOperators: true,
+    isAutoPreviewing: true,
+    index: 0,
+    serverVersion: '4.0.0',
+    fields: [],
+    id: new ObjectId().toHexString(),
+    stageOperator: '$match',
+    stage: `{
+  type: "Terrestrial planet"
+}`
+};
+
+import StageEditor from 'components/stage-editor';
+
+storiesOf('<StageEditor>', module)
+  .addDecorator(story => <ComponentPreview>{story()}</ComponentPreview>)
+  .add('Default', () => {
+    const store = configureStore(INITIAL_STATE);
+    return (
+      <Provider store={store}>
+        <StageEditor {...PROPS} />
+      </Provider>
+    );
+  });
