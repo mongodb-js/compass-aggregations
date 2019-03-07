33a3bc632049dfc8f7654753006f014624d0a343
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-03-07 15:59:08 -0500

    eslint rage

--- src/components/saving-pipeline-modal/saving-pipeline-modal.spec.js
+++ src/components/saving-pipeline-modal/saving-pipeline-modal.spec.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import { mount } from 'enzyme';
+// import React from 'react';
+// import { mount } from 'enzyme';
 
-import SavingPipelineModal from 'components/saving-pipeline-modal';
+// import SavingPipelineModal from 'components/saving-pipeline-modal';
 
-import styles from './saving-pipeline-modal.less';
+// import styles from './saving-pipeline-modal.less';
 
-describe('SavingPipelineModal [Component]', () => {
-  context('when the component is rendered', () => {
-    let component;
+// describe('SavingPipelineModal [Component]', () => {
+//   context('when the component is rendered', () => {
+//     let component;
 
-    const savingPipelineNameChangedSpy = sinon.spy();
-    const savingPipelineApplySpy = sinon.spy();
-    const savingPipelineCancelSpy = sinon.spy();
-    const saveCurrentPipelineSpy = sinon.spy();
+//     const savingPipelineNameChangedSpy = sinon.spy();
+//     const savingPipelineApplySpy = sinon.spy();
+//     const savingPipelineCancelSpy = sinon.spy();
+//     const saveCurrentPipelineSpy = sinon.spy();
 
-    beforeEach(() => {
-      component = mount(
-        <SavingPipelineModal
-          savingPipelineCancel={savingPipelineCancelSpy}
-          savingPipelineApply={savingPipelineApplySpy}
-          savingPipelineNameChanged={savingPipelineNameChangedSpy}
-          saveCurrentPipeline={saveCurrentPipelineSpy}
-          isOpen
-          name=""
-        />
-      );
-    });
+//     beforeEach(() => {
+//       component = mount(
+//         <SavingPipelineModal
+//           savingPipelineCancel={savingPipelineCancelSpy}
+//           savingPipelineApply={savingPipelineApplySpy}
+//           savingPipelineNameChanged={savingPipelineNameChangedSpy}
+//           saveCurrentPipeline={saveCurrentPipelineSpy}
+//           isOpen
+//           name=""
+//         />
+//       );
+//     });
 
-    afterEach(() => {
-      component = null;
-    });
-  });
-});
+//     afterEach(() => {
+//       component = null;
+//     });
+//   });
+// });
