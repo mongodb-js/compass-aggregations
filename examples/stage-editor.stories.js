27ae16fe35c910eefb40046c7258b21bd843f044
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-03-08 10:29:20 -0500

    Stage Editor uses basic examples
    For COMPASS-2888

--- examples/stage-editor.stories.js
+++ examples/stage-editor.stories.js
@@ -7,38 +7,83 @@ import { INITIAL_STATE } from 'modules';
 import { configureStore } from 'utils/configureStore';
 
 import { STAGE_DEFAULTS } from './example-constants.js';
-
 import { ObjectId } from 'bson';
+import ACTION_PROPS from './action-creators';
 
-import { action } from '@storybook/addon-actions';
-
-const BASE_STATE = {
-  ...INITIAL_STATE
-};
+import BASIC_EXAMPLE from './example-basic.js';
+import COMPLEX_EXAMPLE from './example-complex.js';
+import ARRAY_STATS_EXAMPLE from './example-array-stats.js';
+import GROUPED_STATS_EXAMPLE from './example-grouped-stats.js';
 
 const PROPS = {
   ...STAGE_DEFAULTS,
-  runStage: action('runStage'),
-  stageChanged: action('stageChanged'),
-  setIsModified: action('setIsModified'),
+  ...ACTION_PROPS,
   fromStageOperators: true,
   isAutoPreviewing: true,
   index: 0,
   serverVersion: '4.0.0',
   fields: [],
   id: new ObjectId().toHexString(),
-  stageOperator: '$project',
-  stage: `{
-  _id: 0,
-  type: "$type",
-  avg: {$avg: "$value"}
-}`
+  stageOperator: '',
+  stage: '{}'
 };
 
 import StageEditor from 'components/stage-editor';
 
 storiesOf('<StageEditor>', module)
   .addDecorator(story => <ComponentPreview>{story()}</ComponentPreview>)
+  .add('Example > $lookup', () => {
+    const store = configureStore(INITIAL_STATE);
+    const stage = COMPLEX_EXAMPLE.pipeline[0];
+    const props = {
+      ...PROPS,
+      ...stage
+    };
+    return (
+      <Provider store={store}>
+        <StageEditor {...props} />
+      </Provider>
+    );
+  })
+  .add('Example > $project', () => {
+    const store = configureStore(INITIAL_STATE);
+    const stage = ARRAY_STATS_EXAMPLE.pipeline[0];
+    const props = {
+      ...PROPS,
+      ...stage
+    };
+    return (
+      <Provider store={store}>
+        <StageEditor {...props} />
+      </Provider>
+    );
+  })
+  .add('Example > $group', () => {
+    const store = configureStore(INITIAL_STATE);
+    const stage = GROUPED_STATS_EXAMPLE.pipeline[1];
+    const props = {
+      ...PROPS,
+      ...stage
+    };
+    return (
+      <Provider store={store}>
+        <StageEditor {...props} />
+      </Provider>
+    );
+  })
+  .add('Example > $match', () => {
+    const store = configureStore(INITIAL_STATE);
+    const stage = BASIC_EXAMPLE.pipeline[0];
+    const props = {
+      ...PROPS,
+      ...stage
+    };
+    return (
+      <Provider store={store}>
+        <StageEditor {...props} />
+      </Provider>
+    );
+  })
   .add('Default', () => {
     const store = configureStore(INITIAL_STATE);
     return (
